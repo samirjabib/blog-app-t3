ead39ff6e20b76fb5eb345a86e79af399b5543d0
samirdev9669 <samirdev9669@gmail.com>
2023-04-23 17:21:11 -0500

    add next auth

--- src/pages/components/Tags/Tags.tsx
+++ src/pages/components/Tags/Tags.tsx
@@ -4,7 +4,7 @@ export default function Tags(){
             <div>My topics:</div>
             <div className="flex items-center space-x-2 ">
                 {Array.from({length:4}).map((_, index) => (
-                    <div key={index} className="rounded-3xl bg-gray-300 px-4 py-3">
+                    <div key={index} className="rounded-3xl w-full py-3 px-2">
                         tag {index}
                     </div>
                 ))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div className="flex h-full w-full flex-col">
       <Header />
       <section className="grid h-full grid-cols-12 place-items-center">
-        <main className="col-span-8 h-full w-full border-r border-gray-300">
+        <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
           <div className="flex w-full flex-col space-y-4 p-10">
             <div className="flex w-full items-center space-x-4">
               <SearchBar />
