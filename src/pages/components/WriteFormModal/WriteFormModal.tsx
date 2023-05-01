19b9bbc74a8239353bfe8ec210b004e4c33756cf
samirdev9669 <samirdev9669@gmail.com>
2023-05-01 18:23:25 -0500

    add button to the form

--- src/pages/components/WriteFormModal/WriteFormModal.tsx
+++ src/pages/components/WriteFormModal/WriteFormModal.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Modal } from "../Modal";
 import { GlobalContext } from "~/contexts/GlobalContextProvider/context";
+import clsx from "clsx";
 
 export default function WriteFormModal() {
   const { setIsWriteModalOpen, isWriteModalOpen } = useContext(GlobalContext);
@@ -23,6 +24,7 @@ export default function WriteFormModal() {
           name="shortDescription"
           id="shortDescription"
           className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
+          placeholder="Description of the blog"
         />
         <textarea
           name="mainBody"
@@ -32,6 +34,16 @@ export default function WriteFormModal() {
           className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
           placeholder="blog main body"
         />
+        <button
+          className={clsx(
+            "flex items-center space-x-3 rounded border",
+            "border-gray-200 px-4 py-2",
+            "transition hover:border-gray-900 hover:text-gray-900"
+          )}
+          type="submit"
+        >
+          Write
+        </button>
       </form>
     </Modal>
   );
