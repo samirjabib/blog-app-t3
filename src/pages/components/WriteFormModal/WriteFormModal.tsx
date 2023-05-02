96704bfb1282e5ec1d795e7b345d71f269eeeabd
samirdev9669 <samirdev9669@gmail.com>
2023-05-01 19:31:25 -0500

    add zod to form

--- src/pages/components/WriteFormModal/WriteFormModal.tsx
+++ src/pages/components/WriteFormModal/WriteFormModal.tsx
@@ -1,10 +1,38 @@
 import { useContext } from "react";
 import { Modal } from "../Modal";
 import { GlobalContext } from "~/contexts/GlobalContextProvider/context";
-import clsx from "clsx";
+import { clsx } from "clsx";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import type { SubmitHandler } from "react-hook-form";
+
+type WriteFormData = {
+  title: string;
+  description: string;
+  body: string;
+};
+
+const writeFormSchema = z.object({
+  title: z.string().min(3).max(20),
+  description: z.string().min(3).max(60),
+  body: z.string().min(3).max(100),
+});
 
 export default function WriteFormModal() {
   const { setIsWriteModalOpen, isWriteModalOpen } = useContext(GlobalContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<WriteFormData>({
+    resolver: zodResolver(writeFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<WriteFormData> = (data) => console.log(data);
 
   return (
     <Modal isOpen={isWriteModalOpen} onClose={() => setIsWriteModalOpen}>
@@ -12,38 +40,42 @@ export default function WriteFormModal() {
         onSubmit={(e) => e.preventDefault()}
         className="flex flex-col items-center justify-center space-y-4"
       >
+        <p className="mb-10 w-full text-left text-sm text-red-500">
+          {errors.title?.message}
+        </p>
         <input
           type="text"
-          name="title"
-          id="title"
+          {...register("title")}
           className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
           placeholder="Title of the blog"
         />
         <input
           type="text"
-          name="shortDescription"
-          id="shortDescription"
+          {...register("description")}
+          id="description"
           className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
           placeholder="Description of the blog"
         />
         <textarea
-          name="mainBody"
-          id="mainBody"
+          {...register("body")}
+          id="body"
           cols={10}
           rows={10}
           className="h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600"
           placeholder="blog main body"
         />
-        <button
-          className={clsx(
-            "flex items-center space-x-3 rounded border",
-            "border-gray-200 px-4 py-2",
-            "transition hover:border-gray-900 hover:text-gray-900"
-          )}
-          type="submit"
-        >
-          Write
-        </button>
+        <div className="flex w-full justify-end">
+          <button
+            className={clsx(
+              "flex items-center space-x-3 rounded border",
+              "border-gray-200 px-4 py-2",
+              "transition hover:border-gray-900 hover:text-gray-900"
+            )}
+            type="submit"
+          >
+            Write
+          </button>
+        </div>
       </form>
     </Modal>
   );
